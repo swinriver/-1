e97d04478c5c5b46d6cc049f81267bef0da6ef67
swinriver <164745680+swinriver@users.noreply.github.com>
2024-03-24 21:35:08 +0900

    Update FileName.cpp

--- Project3/FileName.cpp
+++ Project3/FileName.cpp
@@ -12,7 +12,7 @@ int main()
 
 	for (k = 0; k < 9; k++)
 	{
-		cout << "(x,y) ÁÂÇ¥ : ";
+		cout << "(x,y) ì¢Œí‘œ : ";
 		cin >> x >> y;
 		board[x][y] = (k % 2 == 0) ? 'X' : 'O';
 		//0 x|1 o|2 x|3 o|4 x|5 o|6 x|7 o| 8 x|
@@ -23,36 +23,36 @@ int main()
 		cout << "---|---|---|" << endl;
 		for (j = 0; j < 3; j++)
 		{
-			if (board[j][0] == board[j][1] && board[j][1] == board[j][2] && board[j][0] != '  ') //0 1 2, 3 4 5, 6 7 8 3°¡Áö
+			if (board[j][0] == board[j][1] && board[j][1] == board[j][2] && board[j][0] != '  ') //0 1 2, 3 4 5, 6 7 8 3ê°€ì§€
 			{
-				cout << board[j][0] << "½Â¸®" << endl;
+				cout << board[j][0] << "ìŠ¹ë¦¬" << endl;
 				break;
 			}
-			if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '  ')//0 3 6, 1 4 7, 2 5 8 3°¡Áö
+			if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '  ')//0 3 6, 1 4 7, 2 5 8 3ê°€ì§€
 			{
-				cout << board[0][j] << "½Â¸®" << endl;
+				cout << board[0][j] << "ìŠ¹ë¦¬" << endl;
 				break;
 			}
-			if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '  ')// 0 4 8 1°¡Áö
+			if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '  ')// 0 4 8 1ê°€ì§€
 			{
-				cout << board[0][0] << "½Â¸®" << endl;
+				cout << board[0][0] << "ìŠ¹ë¦¬" << endl;
 				break;
 			}
-			if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '  ') //2 4 6 1°¡Áö
+			if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '  ') //2 4 6 1ê°€ì§€
 			{
-				cout << board[0][2] << "½Â¸®" << endl;
+				cout << board[0][2] << "ìŠ¹ë¦¬" << endl;
 				break;
 			}
-			break;
 		}
 		if (k == 8)
 		{
-			cout << "¹«½ÂºÎ" << endl;
+			cout << "ë¬´ìŠ¹ë¶€" << endl;
 		}
 	}
 
-	/*0 1 2  ->  012 048 036 147 258 345 678 246
-	3 4 5     if [0][0],[0][1],[0][3]>>'X'||'O' -> braek printf("X½Â || O½Â");
+	/*
+ 	0 1 2  ->  012 048 036 147 258 345 678 246
+	3 4 5     if [0][0],[0][1],[0][3]>>'X'||'O' -> braek printf("XìŠ¹ || OìŠ¹");
 	6 7 8		IF [0][0],[1][1],[2][2]
 	 */
 	return 0;
